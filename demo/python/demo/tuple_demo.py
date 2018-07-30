d3396c3716405e8823762c27d3f1d034c2b58775
YinYichang <yichang1776@163.com>
2018-07-30 09:39:44 +0800

    python学习实例

---
+++ demo/python/demo/tuple_demo.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# tup01 = (1, 2, 3, 4, 5)
+# tup02 = ('jack','mike','mack','cyke')
+# print(tup01[2])
+# print(tup02[2])
+
+# del tup01
+
+# tup02 = ('jack','mike','mack','cyke')
+# print(len(tup02))
+
+# tup02 = ('jack','mike','mack','cyke')
+# list01 = list(tup02)
+# print(list01)
+
+list01 = ['jack', 'mike', 'mack', 'cyke']
+tup03 = tuple(list01)
+print(tup03)
